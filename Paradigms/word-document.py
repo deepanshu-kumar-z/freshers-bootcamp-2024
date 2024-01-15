ed93ed866dcd7715ca796b22236607c429c1a472
deepanshu-kumar-z <156112201+deepanshu-kumar-z@users.noreply.github.com>
2024-01-15 23:15:16 +0530

    Update word-document.py

--- Paradigms/word-document.py
+++ Paradigms/word-document.py
@@ -3,142 +3,114 @@ from abc import ABC, abstractmethod
 class DocumentPart(ABC):
     _name = ''
     _position = ''
-    
     @abstractmethod
     def paint(self):
         pass
-    
     @abstractmethod
     def convert(self, converterClass):
         pass
- 
     @abstractmethod
     def save(self):
         pass
  
 class Header(DocumentPart):
-    def __init__(self, title="DefaultTitle"):
+    def __init__(self, title="Default"):
         self.title = title
-    
     def paint(self):
-        print("Displaying Header titled", self.title)
- 
+        print(self.title)
     def save(self):
         print("Saving Header Content")
-        
     def convert(self, converterClass):
         converterClass.convertHeader(self)
         pass
  
 class Footer(DocumentPart):
-    def __init__(self, text="DefaultText"):
+    def __init__(self, text="Default"):
         self.text = text
-        
     def paint(self):
-        print("Displaying Footer with text", self.text)
- 
+        print(self.text)
     def save(self):
         print("Saving Footer Content")
-        
     def convert(self, converterClass):
         converterClass.convertFooter(self)
         pass
  
 class Hyperlink(DocumentPart):
-    def __init__(self, url="defaultURL.com", text="defaultText"):
+    def __init__(self, url="abc.com", text="default"):
         self.url = url
         self.text = text
-    
     def paint(self):
-        print("Displaying Hyperlink with URL {} and text {}".format(self.url, self.text))
- 
+        print(self.url, self.text)
     def save(self):
         print("Saving Hyperlink Content")
-        
     def convert(self, converterClass):
         converterClass.convertHyperLink(self)
         pass
  
 class Paragraph(DocumentPart):
-    def __init__(self, content="defaultContent"):
+    def __init__(self, content="default"):
         self.content = content
-        
     def paint(self):
-        print("Displaying Paragraph with content", self.content)
- 
+        print(self.content)
     def save(self):
         print("Saving Paragraph Content")
-        
     def convert(self, converterClass):
         converterClass.convertPara(self)
         pass 
  
 class ConverterInterface(ABC):
-    
     @abstractmethod
     def convertHeader(self, header: Header):
         pass
-    
     @abstractmethod
     def convertFooter(self, footer: Footer):
         pass
-    
     @abstractmethod
     def convertHyperLink(self, hyperlink: Hyperlink):
         pass
-    
     @abstractmethod
     def convertPara(self, paragraph: Paragraph):
         pass
     
-class MarkdownConverter(ConverterInterface):
-    def convertHeader(self, header: Header):
-        print("# {}".format(header.title))
-        
-    def convertFooter(self, footer: Footer):
-        print("## {}".format(footer.text))
+class HTMLConverter(ConverterInterface):
+    def convertHeader(self, header : Header):
+        print("<h1>"+header.title+"</h1>")
+    def convertFooter(self, footer : Footer):
+        print("<h2>"+footer.text+"</h2>")
+    def convertHyperLink(self, hyperlink : Hyperlink):
+        print("<a href = {}>{}</a>".format(hyperlink.url, hyperlink.text))
+    def convertPara(self, paragraph : Paragraph):
+        print("<p>{}</p>".format(paragraph.content))
     
-    def convertHyperLink(self, hyperlink: Hyperlink):
-        print("[{}]({})".format(hyperlink.text, hyperlink.url))
-    
-    def convertPara(self, paragraph: Paragraph):
-        print(paragraph.content)
-    
-class MarkdownDocument:
+class WordDocument:
     def __init__(self, documentParts=[]):
         self.documentParts = documentParts
-        
     def addDocumentPart(self, documentPart):
         self.documentParts.append(documentPart)
- 
     def openDocument(self):
         for part in self.documentParts:
             part.paint()
- 
     def saveDocument(self):
         for part in self.documentParts:
             part.save()
- 
+    def transform(self, converterObject):
+        for docPart in self.documentParts:
+            docPart.convert(converterObject)
+            
 def main():
-    headerObj = Header("Title1")
-    hyperlinkObj = Hyperlink("google.co.in", "search")
-    footerObj = Footer("FooterText")
-    paragraphObj = Paragraph("This is a paragraph.")
-    
-    documentPartList = [headerObj, footerObj, hyperlinkObj, paragraphObj] 
-    
-    markdownDoc = MarkdownDocument(documentPartList)
-    
-    MarkdownConverterObj = MarkdownConverter()
-     
-    markdownDoc.openDocument()
+    headerElement = Header("TITLE OF STORY")
+    hyperlinkElement = Hyperlink("www.zeiss.com", "search")
+    footerElement = Footer()
+    paragraphElement = Paragraph("Zeiss is a wonderful organization.")
+    documentPartList = [headerElement, footerElement, hyperlinkElement, paragraphElement] 
+    wordDoc = WordDocument(documentPartList)
+    HTMLConverterElement = HTMLConverter()
+    wordDoc.openDocument()
     print()
-    markdownDoc.saveDocument()
+    wordDoc.saveDocument()
     print()
-    print("Converting to Markdown")
-    print("*"*25)
-
-    for docPart in markdownDoc.documentParts:
-        docPart.convert(MarkdownConverterObj)
+    print("Converting to HTML")
+    print()
+    wordDoc.transform(HTMLConverterElement)
     
 main()
