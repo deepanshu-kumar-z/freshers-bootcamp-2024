150ba3245719848a49fbb31f2c0a0ec641718c8f
deepanshu-kumar-z <156112201+deepanshu-kumar-z@users.noreply.github.com>
2024-01-23 13:35:17 +0530

    Update car-engine.py

--- Paradigms/car-engine.py
+++ Paradigms/car-engine.py
@@ -1,70 +1,87 @@
-class Car:
-    def __init__(self,engineObj, transmissionObj):
-        self.engine = engineObj
-        self.transmission = transmissionObj
-    def getDetails(self):
-        return [self.engine, self.transmission]
-        
-class ITransmission(ABC):
-    @abstractmethod
-    def setTransmission(self,transmissionName):
-        pass
-    @abstractmethod
-    def getTransmission(self):
-        pass
-    
-class Transmission(ITransmission):
-    def setTransmission(self,transmissionName):
-        self.transmission = transmissionName
-
-    def getTransmission(self):
-        return self.transmissionName
+from abc import ABC, abstractmethod
 
 class IEngine(ABC):
     @abstractmethod
-    def setEngine(self,engineName):
-        pass
-    @abstractmethod
-    def getEngine(self):
+    def set_engine(self, engine_name):
         pass
 
-class Engine:
-    def __init__(self, FuelPumpObj, StartupMotorObj):
-        self.fuelPump = FuelPumpObj
-        self.startUpMotor = StartupMotorObj
-    def getDetails(self):
-        return [self.fuelPump, self.startUpMotor]
-        
-    def setEngine(self,engineName):
-        self.engine = engineName
-        
-    def getEngine(self):
-        return self.engineName
-        
+    @abstractmethod
+    def get_engine(self):
+        pass
+
 class IFuelPump(ABC):
     @abstractmethod
-    def setFuelPump(self,fuelPumpName):
+    def pump_fuel(self):
         pass
-    @abstractmethod
-    def getFuelPump(self):
-        pass
- 
-class FuelPump(IFuelPump):
-    def setFuelPump(self,fuelPumpName):
-        self.fuelPump = fuelPumpName
-    def getFuelPump(self):
-        return self.fuelPump
 
 class IStartUpMotor(ABC):
     @abstractmethod
-    def setStartUpMotor(self,startUpMotorName):
+    def start_up(self):
         pass
+
+class ITransmission(ABC):
     @abstractmethod
-    def getStartUpMotor(self):
+    def transmit(self):
         pass
- 
+
+class ICar(ABC):
+    @abstractmethod
+    def get_details(self):
+        pass
+
+class FuelPump(IFuelPump):
+    def pump_fuel(self):
+        pass
+
 class StartUpMotor(IStartUpMotor):
-    def setStartUpMotor(self,startUpMotorName):
-        self.startUpMotor = startUpMotorName
-    def getStartUpMotor(self):
+    def start_up(self):
         pass
+
+class Transmission(ITransmission):
+    def transmit(self):
+        pass
+
+class Engine(IEngine):
+    def __init__(self, fuel_pump, start_up_motor):
+        self.fuel_pump = fuel_pump
+        self.start_up_motor = start_up_motor
+
+    def set_engine(self, engine_name):
+        pass
+
+    def get_engine(self):
+        pass
+
+    def get_details(self):
+        return [self.fuel_pump, self.start_up_motor]
+
+class Car(ICar, Engine, Transmission):
+    def __init__(self, engine, transmission):
+        super().__init__(engine.fuel_pump, engine.start_up_motor)
+        self.engine = engine
+        self.transmission = transmission
+
+    def get_details(self):
+        engine_details = super().get_details() or []
+        return [*engine_details, self.transmission]
+
+class DIContainer:
+    @staticmethod
+    def create_engine():
+        fuel_pump = FuelPump()
+        start_up_motor = StartUpMotor()
+        return Engine(fuel_pump, start_up_motor)
+
+    @staticmethod
+    def create_transmission():
+        return Transmission()
+
+    @staticmethod
+    def create_car():
+        engine = DIContainer.create_engine()
+        transmission = DIContainer.create_transmission()
+        return Car(engine, transmission)
+
+car = DIContainer.create_car()
+details = car.get_details()
+print(details)
